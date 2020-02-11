430fac421cfac2fed28ed9cf9697740c54d06415
Basha <basha502@gmail.com>
2020-02-11 07:22:36 +0530

    imports

--- src/app/dynamic/dynamicForm.component.ts
+++ src/app/dynamic/dynamicForm.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  OnInit,
-  Input,
-  OnChanges,
-  AfterViewInit
-} from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { FormGroup } from "@angular/forms";
 @Component({
   selector: "app-dynamic-form",
